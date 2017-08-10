3de17bf9c10650613c85ce4a84f63251f3e2fbbe
Mykola <kolya182@gmail.com>
2017-08-10 11:39:26 -0700

    Parse req.body

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 //Create instance of express
 let app = express();
-
 let port = 3000;
+let urlencodedParser = bodyParser.urlencoded({extended: false});
 //Serve public folder at app root URL
 app.use(express.static(__dirname + '/../client/dist'));
 app.get('/', express.static('public'));
 
-app.post('/event', (req, res) => {
+app.post('/event', urlencodedParser, (req, res) => {
+  //Parse and store incoming data about event
+  let lat = req.query.latitude;
+  let lng = req.query.langitude;
+  let eventType = req.query.eventType;
+  // let userName = req.query.userName; Keep it for later future
 });
 
 //Starts server and listens for request
